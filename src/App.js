7146f2c85cb7d5c9af8472fcbdedab9c714a0684
Abai <aabasbekov47@gmail.com>
2023-07-18 11:19:15 +0600

    dz_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import ChangeTitle from './components/changeTitle/ChangeTitle';
+import AboutPage from './pages/aboutPage/AboutPage';
+import ContactsPage from './pages/contactsPage/ContactsPage';
+import { Counter } from './pages/counter/Count';
+import MainPage from './pages/mainPage/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MainPage/>
+      <AboutPage/>
+      <ChangeTitle/>
+      <ContactsPage/>
+      <Counter/>
     </div>
   );
 }
-
 export default App;
---
+++ src/redux/reducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+    title: 'old title',
+    aboutTitle: 'old About title',
+    contactTitle: ''
+}
+
+
+export default function reducer (state = initialState, action) {
+
+
+    if (action.type === 'CHANGE_TITLE') {
+        return{...state, title: 'New title'}
+    }
+
+    else if (action.type === 'CHANGE_ABOUT_TITLE') {
+        return{ ...state, aboutTitle: 'New about title'}
+    }
+
+    else if (action.type === 'WITH_PARAMS') {
+        return{ ...state, contactTitle: action.payload}
+    }
+
+    else if (action.type === 'INPUT_TITLE') {
+        return{ ...state, title: action.payload}
+    }
+
+    return state
+}   
